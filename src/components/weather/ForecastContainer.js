6a45df00ce7c24c2c74b46f4a9b4171d695c0da3
TTRIFIC <beckerman.m@sbcglobal.net>
2023-03-02 19:58:56 -0600

    days are working

--- src/components/weather/ForecastContainer.js
+++ src/components/weather/ForecastContainer.js
@@ -4,38 +4,49 @@ import DailyForecast from "./DailyForecast";
 import "./ForecastContainer.css";
 
 const ForecastContainer = ({ weatherData, scheduleData }) => {
+  var days = [
+    "SUNDAY",
+    "MONDAY",
+    "TUESDAY",
+    "WEDNESDAY",
+    "THURSDAY",
+    "FRIDAY",
+    "SATURDAY",
+    "SUNDAY",
+  ];
+
   let dayOneDate = new Date(
-    scheduleData.MRData.RaceTable.Races[0].FirstPractice.date +
-      scheduleData.MRData.RaceTable.Races[0].FirstPractice.time
+    `${scheduleData.MRData.RaceTable.Races[0].FirstPractice.date}
+      ${scheduleData.MRData.RaceTable.Races[0].FirstPractice.time}`
   );
   let dayTwoDate = new Date(
-    scheduleData.MRData.RaceTable.Races[0].Qualifying.date +
-      scheduleData.MRData.RaceTable.Races[0].Qualifying.time
+    `${scheduleData.MRData.RaceTable.Races[0].Qualifying.date}
+      ${scheduleData.MRData.RaceTable.Races[0].Qualifying.time}`
   );
   let dayThreeDate = new Date(
-    scheduleData.MRData.RaceTable.Races[0].date +
-      scheduleData.MRData.RaceTable.Races[0].time
+    `${scheduleData.MRData.RaceTable.Races[0].date}
+      ${scheduleData.MRData.RaceTable.Races[0].time}`
   );
 
-  if (!scheduleData.MRData.RaceTable.Races[0].ThirdPractice.Date) {
+  if (scheduleData.MRData.RaceTable.Races[0].ThirdPractice.Date) {
     dayTwoDate = new Date(
-      scheduleData.MRData.RaceTable.Races[0].SecondPractice.date +
-        scheduleData.MRData.RaceTable.Races[0].SecondPractice.time
+      `${scheduleData.MRData.RaceTable.Races[0].SecondPractice.date}
+        ${scheduleData.MRData.RaceTable.Races[0].SecondPractice.time}`
     );
   }
 
   const dayOne = {
-    day: "Friday",
+    day: days[dayOneDate.getDay()],
     icon: null,
     condition: "Sunny",
   };
   const dayTwo = {
-    day: "Saturday",
+    day: days[dayTwoDate.getDay()],
     icon: null,
     condition: "Clear",
   };
   const dayThree = {
-    day: "Sunday",
+    day: days[dayThreeDate.getDay()],
     icon: null,
     condition: "Cloudy",
   };
