ca8d747561d1edf23bf27b5a4d2a68f1e224c29e
TTRIFIC <beckerman.m@sbcglobal.net>
2023-03-23 13:03:31 -0500

    added precipitation tile

--- src/components/weather/Radar.js
+++ src/components/weather/Radar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import API_KEY from "../../utils/OpenWeatherAPI";
 
 import "./Radar.css";
 
@@ -6,6 +7,11 @@ const Radar = () => {
   return (
     <div className="RadarCard">
       <div className="RadarTitle">RADAR</div>
+      <img
+        className="RadarMap"
+        src={`https://tile.openweathermap.org/map/precipitation_new/1/1/1.png?appid=${API_KEY}`}
+        alt="Radar"
+      />
     </div>
   );
 };
